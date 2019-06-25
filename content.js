923f118370589541bf1ab24e648760a81ed558dc
kamilborecki <k.borecki@clearcode.cc>
2019-06-25 14:16:24 +0200

    step5

--- content.js
+++ content.js
@@ -1,3 +1,5 @@
-(() => {
-
+(async () => {
+  const src = chrome.extension.getURL('lib/injector.js');
+  const injector = await import(src);
+  console.log(injector.injectWord(document.body, 'INJECTOR'));
 })();
