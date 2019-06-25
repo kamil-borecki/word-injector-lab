b18644cef900c92286b0950a9a60ef8df9739df9
kamilborecki <k.borecki@clearcode.cc>
2019-06-25 15:34:41 +0200

    requests

--- background.js
+++ background.js
@@ -23,34 +23,11 @@
             });
           });
           break;
-        case 'state-changed':
-          const {state} = data;
-          chrome.storage.sync.set({state}, () => {
-            reload();
-            chrome.runtime.sendMessage({
-              message: 'new-state',
-              data: {state},
-            });
-          });
-          break;
-        case 'show-badge':
-          const {count} = data;
-          showBadge(count);
-          break;
       }
     });
 
   function reload() {
-    showBadge(0);
     chrome.tabs.sendMessage(currentTabId, {message: 'reload'});
   }
 
-  function showBadge(count) {
-    let text = '';
-    if (count !== 0) {
-      text = count > 999 ? `999+` : count.toString();
-    }
-    chrome.browserAction.setBadgeBackgroundColor({color: '#ff4555'});
-    chrome.browserAction.setBadgeText({text});
-  }
 })();
--- content.js
+++ content.js
@@ -1,23 +1,13 @@
-(async () => {
-  const src = chrome.extension.getURL('lib/injector.js');
-  const injector = await import(src);
+(() => {
 
-  chrome.runtime.onMessage.addListener(
-    request => {
-      if (request.message === 'reload') {
-        location.reload();
-      }
-    });
+  makeRequest();
 
-  chrome.storage.sync.get(({word, state}) => {
-    if (!word || !state) {
-      return;
-    }
-    const wordsCount = injector.injectWord(document.body, word);
-    
-    chrome.runtime.sendMessage({
-      message: 'show-badge',
-      data: {count: wordsCount},
-    });
-  })
+  function makeRequest() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(resp => resp.json())
+      .then(resp => {
+        console.log(resp);
+      });
+  }
+
 })();
