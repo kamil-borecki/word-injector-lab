0b1caf829a93d4953b8b4ad370d5f17dd3dfd0b3
kamilborecki <k.borecki@clearcode.cc>
2019-06-25 14:35:39 +0200

    step6

--- content.js
+++ content.js
@@ -1,5 +1,11 @@
 (async () => {
   const src = chrome.extension.getURL('lib/injector.js');
   const injector = await import(src);
-  console.log(injector.injectWord(document.body, 'INJECTOR'));
+
+  chrome.storage.sync.get(({word, state}) => {
+    if (!word || !state) {
+      return;
+    }
+    injector.injectWord(document.body, word)
+  })
 })();
